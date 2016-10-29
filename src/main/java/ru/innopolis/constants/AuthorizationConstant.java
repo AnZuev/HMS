ce89479e5f1dc5c321d47fd486d90a334a19861e
Denis <Den1-1@mail.ru>
2016-10-29 17:30:17 +0300

    Добавлен фильт проверки доступа к информации, требующей аутентификации.

---
+++ src/main/java/ru/innopolis/constants/AuthorizationConstant.java
@@ -0,0 +1,10 @@
+package ru.innopolis.constants;
+
+/**
+ * Создано: Денис
+ * Дата:  29.10.2016
+ * Описание: Константы для аутентификации
+ */
+public class AuthorizationConstant {
+    public static final String AUTHORIZATION_KEY = "AUTHORIZATION_KEY";
+}
---
+++ src/main/java/ru/innopolis/filters/AuthorizationFilter.java
@@ -0,0 +1,32 @@
+package ru.innopolis.filters;
+
+import org.apache.commons.lang3.BooleanUtils;
+import ru.innopolis.constants.AuthorizationConstant;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Создано: Денис
+ * Дата:  29.10.2016
+ * Описание: Фильт предназначен для проверки аутентифицировался ли пользователь или нет.
+ */
+public class AuthorizationFilter implements Filter {
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession(false);
+        if (session != null){
+            Boolean attribute = (Boolean) session.getAttribute(AuthorizationConstant.AUTHORIZATION_KEY);
+            if (BooleanUtils.isTrue(attribute)){
+                chain.doFilter(request, response);
+            }else {
+                HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+                httpServletResponse.sendRedirect(httpRequest.getContextPath());
+            }
+        }
+    }
+}
