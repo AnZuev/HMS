adbc17cdc8918d6f5efcfd60a776ca7f2a0d2863
Denis <Den1-1@mail.ru>
2016-11-10 14:12:59 +0300

    Hotel-40. Разработать механизма просмотра сотрудников для владельца отеля. Докоммит.

---
+++ Employee/src/main/java/ru/innopolis/models/EmployeeResponseModel.java
@@ -0,0 +1,63 @@
+package ru.innopolis.models;
+
+/**
+ * Создано: Денис
+ * Дата:  06.11.2016
+ * Описание: Модель ответа на запрос получения списка владельцев
+ */
+public class EmployeeResponseModel {
+    private Long id;
+    private String firstName;
+    private String secondName;
+    private String fatherName;
+    private String mail;
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
